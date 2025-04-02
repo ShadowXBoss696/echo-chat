047eb59fea6b1fb7ea7f27464850c3a363da45b7
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2025-04-02 22:22:12 +0530

    first commit

---
+++ eca-service-discovery/src/test/java/com/shadowx/echochat/discovery/EurekaServerApplicationTest.java
@@ -0,0 +1,12 @@
+package com.shadowx.echochat.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaServerApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
